7ad8e181c2b0e952ecfbc05847c9c9773c87e40d
rishirt <rishithulasiramanr@gmail.com>
2019-02-14 04:49:33 +0530

    added openCV code

--- index.js
+++ index.js
@@ -23,42 +23,29 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'))
 
-/**
- * Set up Handlebars templating 
- */
+
 app.engine('.hbs', exphbs( { extname: '.hbs', defaultLayout: 'default' } ) );
 app.set( 'view engine', '.hbs' );
 
-/**
- * Default page; simply renders a file upload form 
- */
+
 app.get('/', function( req, res, next ) {
 
 	return res.render('index');
 
 });
 
-/**
- * POST callback for the file upload form. This is where the magic happens. 
- */
+
 app.post('/upload', upload.single('file'), function(req, res, next){
 	
-	// Generate a filename; just use the one generated for us, plus the appropriate extension
+	
 	var filename = req.file.filename + exts[req.file.mimetype]
-		// and source and destination filepaths
 		, src = __dirname + '/' + req.file.path
 		, dst = __dirname + '/public/images/' + filename;
 
-	/**
-	 * Go through the various steps
-	 */
 	async.waterfall(
 		[
 			function( callback ) {
 				
-				/**
-				 * Check the mimetype to ensure the uploaded file is an image
-				 */
 				if (!_.contains(
 					[
 						'image/jpeg',
@@ -77,16 +64,10 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 			},
 			function( callback ) {
 
-				/**
-			 	* Get some information about the uploaded file	 
-			 	*/
 				easyimg.info( src ).then(
 		
 					function(file) {     
 					
-						/**
-						 * Check that the image is suitably large			 
-						 */
 						if ( ( file.width < 960 ) || ( file.height < 300 ) ) {            
 							
 							return callback( new Error( 'Image must be at least 640 x 300 pixels' ) );
@@ -99,9 +80,7 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 			},
 			function( callback ) {
 				
-				/**
-				 * Resize the image to a sensible size				 
-				 */
+				
 				easyimg.resize(
           {
             width      :   960,            
@@ -117,17 +96,13 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 			},
 			function( callback ) {
 
-				/**
-         * Use OpenCV to read the (resized) image        	 
-         */
+				
 				cv.readImage( dst, callback );
 
 			},
 			function( im, callback ) {
 
-				/**
-				 * Run the face detection algorithm	
-				 */
+				
 				im.detectObject( cv.FACE_CASCADE, {}, callback );
 
 			}
@@ -135,10 +110,7 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 		],
 		function( err, faces ) {
 			
-			/**
-			 * If an error occurred somewhere along the way, render the 
-			 * error page.
-			 */
+			
 			if ( err ) {
 				
 				return res.render(
